ddfb4420e0565e3e59dfe3d6c927718db649f6d8
victormosqueraconejo <victormanuelmosqueraconejo@gmail.com>
2025-03-28 20:31:29 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    base: '/home-preview-github/', // reemplaza "nombre-del-repositorio" con el nombre de tu repositorio
+  build: {
+    outDir: 'dist',
+  },
 })
